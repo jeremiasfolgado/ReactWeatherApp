52b2354401df2d546da22f7e98faf85d37b22f33
jeremiasfolgado <jeremias.folgado@gmail.com>
2021-07-24 21:14:31 -0300

    design

--- src/components/CityDetail/CityDetail.js
+++ src/components/CityDetail/CityDetail.js
@@ -15,30 +15,30 @@ const CityDetail = ({city}) =>{
     return (
         <div className="main-container">
             <div className="detail-container">
-            <div className="icon-title">
-                <h4>{city.name}</h4>
-                <img src={`https://openweathermap.org/img/wn/${city.icon}@4x.png`} alt="weather icon"></img>
-                <span>{city.description}</span>
-            </div>
-            <div className="aditional-info">
-                <div className="more-info">
-                    <span>Sensación termica</span>
-                    <span>{city.feelsLike} °</span>
-                    
+                <div className="icon-title">
+                    <h4>{city.name}</h4>
+                    <img src={`https://openweathermap.org/img/wn/${city.icon}@4x.png`} alt="weather icon"></img>
+                    <span>{city.description}</span>
                 </div>
-                <div className="more-info">
-                    <span>Humedad</span>
-                    <span>{city.humidity}</span>
-                </div>
+                <div className="aditional-info">
+                    <div className="more-info">
+                        <span>Sensación térmica</span>
+                        <span>{city.feelsLike} °</span>
+                        
+                    </div>
+                    <div className="more-info">
+                        <span>Humedad / 100 </span>
+                        <span>{city.humidity}</span>
+                    </div>
+                        
                     
-                
-                <div className="more-info">
-                    <span>Viento</span>
-                    
-                    <span>{city.wind} km/h</span>
-                    
-                    
-                </div>
+                    <div className="more-info">
+                        <span>Viento</span>
+                        
+                        <span>{city.wind} km/h</span>
+                        
+                        
+                    </div>
                     
             </div>
 
